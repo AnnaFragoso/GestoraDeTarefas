2f5febac4899936afb4c53489a75e5ba684bb071
AnnaFragoso <anna.barbara.fragoso@hotmail.com>
2019-02-22 22:38:32 -0300

    Inicio Singleton, tela de adicionar, snackbar de salvar tarefa

--- app/src/main/java/com/example/desen/gestoradetarefas/TarefasFragment.java
+++ app/src/main/java/com/example/desen/gestoradetarefas/TarefasFragment.java
@@ -3,9 +3,11 @@ package com.example.desen.gestoradetarefas;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -23,7 +25,24 @@ public class TarefasFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tarefas, container, false);
+        View v = inflater.inflate(R.layout.fragment_tarefas, container, false);
+
+        container = (ViewGroup) v.findViewById(R.id.container);
+
+        Tarefas tar = TarefaSingleton.getInstance().getTarefas();
+
+        if (tar != null) {
+            CardView cardView = (CardView) LayoutInflater.from(getActivity())
+                    .inflate(R.layout.card, container, false);
+            cardView.findViewById(R.id.titulo);
+            TextView titulo = (TextView) cardView.findViewById(R.id.titulo);
+            TextView mensagem = (TextView) cardView.findViewById(R.id.mensagem);
+
+            titulo.setText(tar.getTitulo());
+            mensagem.setText(tar.getDescricao());
+            container.addView(cardView);
+        }
+       return v;
     }
 
 }
